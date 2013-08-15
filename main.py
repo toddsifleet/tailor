bd75b1bcca1741d7a5d1bf9cee1c7e7c12091400
Todd Sifleet <todd.sifleet@gmail.com>
2013-08-15 13:04:02 -0400

    Add ignore/match logic
    Let users define regexes that are either inclusive or exclusive.e

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import threading
 import Queue
 import argparse
 from itertools import product
+import re
+import time
+from functools import partial
 
 class Tailor(threading.Thread):
-  def __init__(self, queue, server, file, lock):
+  def __init__(self, queue, lock, server, file, match = None, ignore = None):
     self.lock = lock
     threading.Thread.__init__(self)
     self.server = server
@@ -13,11 +16,16 @@ class Tailor(threading.Thread):
     self.queue = queue
     self.daemon = True
     self.running = True
+    self.match = match
+    self.ignore = ignore
     self.start()
 
 
   def put_in_queue(self, data):
-    self.queue.put((self.server, self.file, data))
+    if self.ignore and re.search(self.ignore, data):
+      return
+    if not self.match or re.search(self.match, data):
+      self.queue.put((self.server, self.file, data))
 
   def connect(self):
     #I was getting some weird results where one thread would block out another
@@ -57,13 +65,15 @@ class Tailor(threading.Thread):
       self._Thread__stop()
       return self
 
-def run(files, servers):
+def run(args):
   queue = Queue.Queue()
   lock = threading.Lock()
-  trailers = [Tailor(queue, server, file, lock) for server, file in product(servers, files)]
-  colors = { f: (91 + i) % 100 for i,f in enumerate(files) } if len(files)>1 else None
+  T = partial(Tailor, queue, lock)
+  server_file_combos = product(args.servers, args.files)
+  trailers = [T(server, file, args.match, args.ignore) for server, file in server_file_combos]
+  colors = { f: (91 + i) % 100 for i,f in enumerate(args.files) } if len(args.files)>1 else None
   if colors:
-    for f in files:
+    for f in args.files:
       print_with_color(f, colors[f])
   tail(queue, colors, trailers)
 
@@ -73,6 +83,9 @@ def print_with_color(data, color):
 def tail(queue, colors, trailers):
   try:
     while True:
+      if queue.empty():
+        time.sleep(.5)
+        continue
       server, file, data = queue.get()
       if colors:
         print_with_color(data + "\r", colors[file])
@@ -81,14 +94,26 @@ def tail(queue, colors, trailers):
 
   except KeyboardInterrupt:
     for t in trailers:
-      t.stop().join()
+      t.stop()
 
 def parse_args():
   parser = argparse.ArgumentParser(description = 'Tail a file[s] across locally and/or across multiple servers')
+
+  parser.add_argument('-i', '--ignore',
+    default = None,
+    help = 'a regex string to ignore (similar to: tail -f <file> | grep -v <ignore>)'
+  )
+
+  parser.add_argument('-m', '--match',
+    default = None,
+    help = 'a regex string to match(similar to: tail -f <file> | grep <match>)'
+  )
+
   parser.add_argument('files',
     type = str,
     help = 'The path to the file you want to tail'
   )
+
   parser.add_argument('servers',
     type = str,
     default = 'local',
@@ -98,9 +123,13 @@ def parse_args():
 
   args = parser.parse_args()
 
-  servers = map(lambda x: x.strip(), args.servers.split(','))
-  files = map(lambda x: x.strip(), args.files.split(','))
-  return filter(bool, files), filter(bool, servers)
+  args.servers = filter(bool, map(lambda x: x.strip(), args.servers.split(',')))
+  args.files = filter(bool, map(lambda x: x.strip(), args.files.split(',')))
+  if args.ignore is not None:
+    args.ignore = re.compile(args.ignore, re.I)
+  if args.match is not None:
+    args.match = re.compile(args.match, re.I)
+  return args
 
 if __name__ == "__main__":
-  run(*parse_args())
+  run(parse_args())
